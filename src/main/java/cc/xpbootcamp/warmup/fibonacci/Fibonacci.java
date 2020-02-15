6a04e0ea80c3f2828bea6a3eaa8611be04fd70ca
zdazx <zhangduo0227@163.com>
2020-02-15 18:51:43 +0800

    [zhangduo] feat: update return type of calculate method and add judging result with max value of integer

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -1,14 +1,14 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
 public class Fibonacci {
-    public int calculate(int position) {
+    public Object calculate(int position) {
         if (position == 1) {
             return 1;
         }
 
-        int first = 0;
-        int second = 1;
-        int res = 1;
+        long first = 0;
+        long second = 1;
+        long res = 1;
 
         for (int i = 2; i <= position; i++) {
             res = first + second;
@@ -16,6 +16,9 @@ public class Fibonacci {
             second = res;
         }
 
+        if (Integer.MAX_VALUE > res) {
+            return (int)res;
+        }
         return res;
     }
 }
