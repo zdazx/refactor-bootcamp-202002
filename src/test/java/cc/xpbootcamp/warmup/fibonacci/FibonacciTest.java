4e881e2a8ff1a8a2688a319b0f8e171694bd921b
zdazx <zhangduo0227@163.com>
2020-02-15 14:35:52 +0800

    [zhangduo] feat: refactor by extracting a filed and initialing it in setup

--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -1,21 +1,28 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class FibonacciTest {
+
+    private Fibonacci fibonacci;
+
+    @BeforeEach
+    void setUp() {
+        fibonacci = new Fibonacci();
+    }
+
     @Test
     public void should_return_1_when_calculate_given_position_is_1() {
-        Fibonacci fibonacci = new Fibonacci();
         int result = fibonacci.calculate(1);
         assertThat(result, is(1));
     }
 
     @Test
     public void should_return_1_when_calculate_given_position_is_2() {
-        Fibonacci fibonacci = new Fibonacci();
         int result = fibonacci.calculate(2);
         assertThat(result, is(1));
     }
