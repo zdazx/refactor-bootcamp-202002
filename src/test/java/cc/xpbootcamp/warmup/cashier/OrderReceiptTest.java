185069f879baf90a4220fca4c47efdb0279afc72
zdazx <zhangduo0227@163.com>
2020-02-19 17:12:54 +0800

    [zhangduo] refactor: update unit test

--- src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
+++ src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
@@ -1,43 +1,64 @@
 package cc.xpbootcamp.warmup.cashier;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
 class OrderReceiptTest {
-    @Test
-    void shouldPrintCustomerInformationOnOrder() {
-        Order order = new Order("Mr X", "Chicago, 60601", new ArrayList<LineItem>(), new Date());
-        OrderReceipt receipt = new OrderReceipt(order);
 
-        String output = receipt.printReceipt();
+    private SimpleDateFormat sdf;
 
-
-        assertThat(output, containsString("Mr X"));
-        assertThat(output, containsString("Chicago, 60601"));
+    @BeforeEach
+    void setUp() {
+        sdf = new SimpleDateFormat("yyyy年MM月dd日 EEEE", Locale.CHINA);
     }
 
     @Test
-    public void shouldPrintLineItemAndSalesTaxInformation() {
+    void shouldPrintLineItemAndSalesTaxInfo() throws ParseException {
         List<LineItem> lineItems = new ArrayList<LineItem>() {{
-            add(new LineItem("milk", 10.0, 2));
-            add(new LineItem("biscuits", 5.0, 5));
-            add(new LineItem("chocolate", 20.0, 1));
+            add(new LineItem("巧克力", 21.50, 2));
+            add(new LineItem("小白菜", 10.00, 1));
         }};
-        OrderReceipt receipt = new OrderReceipt(new Order(null, null, lineItems, new Date()));
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 EEEE", Locale.CHINA);
+        Date date = sdf.parse("2020年2月19日 星期三");
+        OrderReceipt receipt = new OrderReceipt(new Order(null, null, lineItems, date));
 
         String output = receipt.printReceipt();
 
-        assertThat(output, containsString("milk, 10.0 x 2, 20.0\n"));
-        assertThat(output, containsString("biscuits, 5.0 x 5, 25.0\n"));
-        assertThat(output, containsString("chocolate, 20.0 x 1, 20.0\n"));
-        assertThat(output, containsString("税额: 6.5"));
-        assertThat(output, containsString("总价: 71.5"));
+        assertThat(output, containsString("巧克力, 21.50 x 2, 43.00\n"));
+        assertThat(output, containsString("小白菜, 10.00 x 1, 10.00\n"));
+        assertThat(output, containsString("税额: 5.30"));
+        assertThat(output, containsString("折扣: 1.17"));
+        assertThat(output, containsString("总价: 57.13"));
+
+    }
+
+    @Test
+    public void shouldPrintLineItemAndSalesTaxInformation() throws ParseException {
+        List<LineItem> lineItems = new ArrayList<LineItem>() {{
+            add(new LineItem("milk", 10.00, 2));
+            add(new LineItem("biscuits", 5.00, 5));
+            add(new LineItem("chocolate", 20.00, 1));
+        }};
+        Date date = sdf.parse("2020年2月18日 星期二");
+        OrderReceipt receipt = new OrderReceipt(new Order(null, null, lineItems, date));
+
+        String output = receipt.printReceipt();
+
+        assertThat(output, containsString("milk, 10.00 x 2, 20.00\n"));
+        assertThat(output, containsString("biscuits, 5.00 x 5, 25.00\n"));
+        assertThat(output, containsString("chocolate, 20.00 x 1, 20.00\n"));
+        assertThat(output, containsString("税额: 6.50"));
+        assertThat(output, containsString("总价: 71.50"));
     }
 
 }
