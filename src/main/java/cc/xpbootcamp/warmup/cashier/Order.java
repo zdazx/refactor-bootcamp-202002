d46336cf3121fc2227ca51823a52d60c61b32189
zdazx <zhangduo0227@163.com>
2020-02-19 17:14:17 +0800

    [zhangduo] refactor: add a filed that whether the order have discount or not

--- src/main/java/cc/xpbootcamp/warmup/cashier/Order.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/Order.java
@@ -3,18 +3,21 @@ package cc.xpbootcamp.warmup.cashier;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class Order {
     private String cName;
     private String addr;
     private List<LineItem> lineItems;
     private String date;
+    private boolean isHaveDiscount;
 
     public Order(String cName, String addr, List<LineItem> lineItems, Date date) {
         this.cName = cName;
         this.addr = addr;
         this.lineItems = lineItems;
-        this.date = new SimpleDateFormat("yyyy年MM月dd日 E").format(date);
+        this.date = new SimpleDateFormat("yyyy年MM月dd日 EEEE", Locale.CHINA).format(date);
+        this.isHaveDiscount = new SimpleDateFormat("EEEE", Locale.CHINA).format(date).equals("星期三");
     }
 
     public String getCustomerName() {
@@ -29,7 +32,7 @@ public class Order {
         return lineItems;
     }
 
-    public String getDate() {
-        return date;
-    }
+    public String getDate() { return date; }
+
+    public boolean isHaveDiscount() { return isHaveDiscount; }
 }
