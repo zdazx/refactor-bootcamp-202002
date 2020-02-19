a0dc3e00312c474621d0f1b53dba03df298cbd06
zdazx <zhangduo0227@163.com>
2020-02-19 15:41:31 +0800

    [zhangduo] refactor: add a date field in Order class

--- src/main/java/cc/xpbootcamp/warmup/cashier/Order.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/Order.java
@@ -1,16 +1,20 @@
 package cc.xpbootcamp.warmup.cashier;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class Order {
     private String cName;
     private String addr;
     private List<LineItem> lineItems;
+    private String date;
 
-    public Order(String cName, String addr, List<LineItem> lineItems) {
+    public Order(String cName, String addr, List<LineItem> lineItems, Date date) {
         this.cName = cName;
         this.addr = addr;
         this.lineItems = lineItems;
+        this.date = new SimpleDateFormat("yyyy年MM月dd日 E").format(date);
     }
 
     public String getCustomerName() {
@@ -23,5 +27,9 @@ public class Order {
 
     public List<LineItem> getLineItems() {
         return lineItems;
+    }
+
+    public String getDate() {
+        return date;
     }
 }
