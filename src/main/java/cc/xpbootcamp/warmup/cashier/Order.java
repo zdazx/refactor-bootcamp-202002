1006c5efe7f25fb98fe150b8bd258dbaa936e5ce
zdazx <zhangduo0227@163.com>
2020-02-19 18:28:50 +0800

    [zhangduo] refactor: add three fiels to record total tax, discount and amount

--- src/main/java/cc/xpbootcamp/warmup/cashier/Order.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/Order.java
@@ -11,6 +11,9 @@ public class Order {
     private List<LineItem> lineItems;
     private String date;
     private boolean isHaveDiscount;
+    private double totalTax;
+    private double totalAmount;
+    private double totalDiscount;
 
     public Order(String cName, String addr, List<LineItem> lineItems, Date date) {
         this.cName = cName;
@@ -18,6 +21,7 @@ public class Order {
         this.lineItems = lineItems;
         this.date = new SimpleDateFormat("yyyy年MM月dd日 EEEE", Locale.CHINA).format(date);
         this.isHaveDiscount = new SimpleDateFormat("EEEE", Locale.CHINA).format(date).equals("星期三");
+        calculateBill();
     }
 
     public String getCustomerName() {
@@ -35,4 +39,22 @@ public class Order {
     public String getDate() { return date; }
 
     public boolean isHaveDiscount() { return isHaveDiscount; }
+
+    public double getTotalTax() { return totalTax; }
+
+    public double getTotalAmount() { return totalAmount; }
+
+    public double getTotalDiscount() { return totalDiscount; }
+
+    private void calculateBill() {
+        for (LineItem lineItem : this.lineItems) {
+            this.totalTax += lineItem.totalAmount() * .10;
+            this.totalAmount += lineItem.totalAmount();
+        }
+        this.totalAmount += this.totalTax;
+        if (this.isHaveDiscount) {
+            this.totalDiscount = this.totalAmount * .02;
+            this.totalAmount *= .98;
+        }
+    }
 }
