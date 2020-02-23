1d4598caa5040a476a92263df54c572fd54a7fb7
zdazx <zhangduo0227@163.com>
2020-02-23 21:07:57 +0800

    [zhangduo] refactor: add a unit test

--- src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
+++ src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
@@ -12,6 +12,7 @@ import java.util.Locale;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 
 class OrderReceiptTest {
 
@@ -39,6 +40,34 @@ class OrderReceiptTest {
         assertThat(output, containsString("税额: 5.30"));
         assertThat(output, containsString("折扣: 1.17"));
         assertThat(output, containsString("总价: 57.13"));
+
+    }
+
+    @Test
+    public void shouldPrintAllOrderInfo() throws ParseException {
+        List<LineItem> lineItems = new ArrayList<LineItem>() {{
+            add(new LineItem("巧克力", 21.50, 2));
+            add(new LineItem("小白菜", 10.00, 1));
+        }};
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 EEEE", Locale.CHINA);
+        Date date = sdf.parse("2020年2月19日 星期三");
+        OrderReceipt receipt = new OrderReceipt(new Order(lineItems, date));
+
+        String output = receipt.printReceipt();
+
+        String expected =
+                "===== 老王超市，值得信赖 ======\n" +
+                "\n" +
+                "2020年02月19日 星期三\n" +
+                "\n" +
+                "巧克力, 21.50 x 2, 43.00\n" +
+                "小白菜, 10.00 x 1, 10.00\n" +
+                "-----------------------------------\n" +
+                "税额: 5.30\n" +
+                "折扣: 1.17\n" +
+                "总价: 57.13\n";
+
+        assertThat(output, is(expected));
 
     }
 
